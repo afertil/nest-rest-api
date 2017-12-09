cfeba94979a69e641496f66a7e6424b4131f9f29
Anthony Fertil <anthony.fertil@hotmail.fr>
2017-12-09 11:12:55 +0100

    Enable cors for app

--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import * as express from 'express';
 import * as bodyParser from 'body-parser';
+import * as cors from 'cors';
 
 import { ApplicationModule } from './modules/app.module';
 
-const app = express();
+const exp = express();
 
 // Express middleware.
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+exp.use(bodyParser.json());
+exp.use(bodyParser.urlencoded({ extended: false }));
+exp.use(cors());
 
 async function bootstrap() {
-	const app = await NestFactory.create(ApplicationModule);
+	const app = await NestFactory.create(ApplicationModule, exp);
 	await app.listen(3000);
 }
 bootstrap();
